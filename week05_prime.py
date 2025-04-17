b004dbd9e69010f6c48c2b64a878bb00d0a91718
void <202330235@email.daelim.ac.kr>
2025-04-17 11:16:42 +0900

    v0.9 prime number

---
+++ week05_prime.py
@@ -0,0 +1,16 @@
+def is_prime(number) :
+    if number >= 2:
+        for i in range(2, int(number**0.5) +1) :
+            if number % i == 0:
+                return False
+            # print(i, end=" ")
+    else:
+        return False
+    return True
+
+n1 = int(input())
+n2 = int(input())
+
+for i in range(n1, n2+1) :
+    if is_prime(i) :
+        print(i, end=" ")
