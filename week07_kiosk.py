e60979abcd4598e0ea0ce734306c7ffac328bd72
void <202330235@email.daelim.ac.kr>
2025-04-17 12:56:45 +0900

    v2.1 kiosk

--- week07_kiosk.py
+++ week07_kiosk.py
@@ -35,14 +35,17 @@ def print_receipt() -> None:
             print(f"{drinks[i]:^20}{price[i]:^6}{amounts[i]:^6}{price[i] * amounts[i]:^6}")
 
 while True :
-    menu = int(input(display_menu()))
-    if len(drinks) >= menu >= 1:
-        order_process(int(menu) - 1)
-    elif menu == len(drinks) + 1 :
-        print("주문을 종료합니다")
-        break
-    else:
-        print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 멘에서 골라주세요.")
+    try :
+        menu = int(input(display_menu()))
+        if len(drinks) >= menu >= 1:
+            order_process(int(menu) - 1)
+        elif menu == len(drinks) + 1 :
+            print("주문을 종료합니다")
+            break
+        else:
+            print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요.")
+    except ValueError :
+        print(f"문자를 입력할 수 없습니다. 숫자를 입력해주세요.")
 
 print(f"총 주문 금액 : {total_price}원")
 print_receipt()
