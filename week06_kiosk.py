d3d0ab2b86ee430132c95928617a1e7cde0e12fe
void <202330235@email.daelim.ac.kr>
2025-04-17 11:55:00 +0900

    v1.1 kiosk

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -4,6 +4,16 @@ price = [1500, 2500, 4000]
 amounts = [0, 0, 0]
 total_price = 0
 
+def order_process(idx):
+    """
+    주문처리 함수 1) 주문 디스플레이 2) 총 주문 금액 누산 3) 주문 품목 수량
+    return : 없음
+    """
+    global total_price
+    print(f"{drinks[idx]}를 주문하셨습니다. 가격은 {price[idx]}원입니다.")
+    total_price = total_price + price[idx]
+    amounts[idx] = amounts[idx] + 1
+
 menu_texts = ""
 for j in range(len(drinks)) :
     menu_texts = menu_texts + f"{j+1}) {drinks[j]} {price[j]}원 "
@@ -12,17 +22,11 @@ menu_texts = menu_texts + f"{len(drinks)+1}) 주문 종료 : "
 while True :
     menu = input(menu_texts)
     if menu == "1" :
-        print(f"{drinks[0]}를 주문하셨습니다. 가격은 {price[0]}원입니다.")
-        total_price = total_price + price[0]
-        amounts[0] = amounts[0] + 1
+        order_process(int(menu) - 1)
     elif menu == "2":
-        print(f"{drinks[1]}를 주문하셨습니다. 가격은 {price[1]}원입니다")
-        total_price = total_price + price[1]
-        amounts[1] = amounts[1] + 1
+        order_process(int(menu) - 1)
     elif menu == "3":
-        print(f"{drinks[2]}를 주문하셨습니다. 가격은 {price[2]}원입니다")
-        total_price = total_price + price[2]
-        amounts[2] = amounts[2] + 1
+        order_process(int(menu) - 1)
     elif menu == "4" :
         print("주문을 종료합니다")
         break
