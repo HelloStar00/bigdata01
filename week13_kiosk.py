062a7aa95f2b2f482fd79f9dff5bb88ffdf992fb
void <202330235@email.daelim.ac.kr>
2025-05-29 10:38:47 +0900

    v3.9 koisk

--- week13_kiosk.py
+++ week13_kiosk.py
@@ -4,9 +4,13 @@ import requests
 
 if __name__ == "__main__":
 
-    url = f"http://wttr.in/seoul?format=%C+%t&lang=ko"
+    # url = f"http://wttr.in/seoul?format=%C+%t&lang=ko"
+    url = f"http://naver.com/kim" # 404 page not found
     response = requests.get(url)
-    print(response.text.strip())
+    if response.status_code == 200:
+        print(response.text.strip())
+    else:
+        print(f"상태코드 : {response.status_code}")
     k.run()
     k.print_receipt()
     k.print_ticket_number()
